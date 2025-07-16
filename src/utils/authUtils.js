b9ed4a086ff73c6dd73a87a9d932c427442f309f
Jamh4949 <jose.armando.martinez@correounivalle.edu.cogit config --global user.email>
2025-07-16 18:14:23 -0500

    Pages de cambios de roles y creación de usuarios

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -1,12 +1,14 @@
 export const normalizeUser = (user, formData = {}) => {
-    return {
-      id: user.id,
-      cedula: user.cedula || formData.cedula || '',
-      email: user.email || formData.email || '',
-      nombre: user.nombre || formData.nombre || 'Usuario',
-      es_profesional: user.es_profesional || formData.es_profesional || false,
-    };
+  return {
+    id: user.id,
+    cedula: user.cedula || formData.cedula || '',
+    email: user.email || formData.email || '',
+    nombre: user.nombre || formData.nombre || 'Usuario',
+    es_profesional: user.es_profesional || formData.es_profesional || false,
+    rol: user.rol || formData.rol || '',
+    is_admin: user.is_admin || formData.is_admin || false,
   };
+};
   
   export const updateLocalStorage = (access, refresh, user) => {
     localStorage.setItem('token', access);
